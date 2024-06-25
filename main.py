8c2f0877104ead6fe6310f6d259af673bbc0aa6c
TheGenocides <luke.genesis.hyder@gmail.com>
2024-06-25 14:06:52 +0700

    embed response

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import discord
 import os
+import datetime
 from googletrans import Translator
 from dotenv import load_dotenv
 from discord.ext import commands
 
+# Load the .env file, you need to make a .env file with the TOKEN variable 
 load_dotenv()
 
 bot = commands.Bot(command_prefix="ec!", intents=discord.Intents.all())
@@ -19,11 +21,17 @@ async def ping(ctx):
 
 @bot.command(aliases=["trans", "tl"])
 async def translate(ctx, *, text = None):
-    if not text:
-        text = (await ctx.channel.fetch_message(ctx.message.reference.message_id)).content
+    msg = await ctx.reply("Translating....")
+    text = text if text else ((await ctx.channel.fetch_message(ctx.message.reference.message_id)).content)
     source = translator.detect(text).lang
     translated_to = "en" if source == "id" else "id"
     translated_text = translator.translate(text, src=source, dest = translated_to).text
-    await ctx.reply(translated_text)
+    embed = discord.Embed(
+        title=f"{translated_to} -> {'en' if translated_to == 'id' else 'id'}",
+        description=translated_text,
+        colour=discord.Color.blurple(),
+        timestamp=datetime.datetime.now()
+    )
+    await msg.edit(embed=embed)
 
 bot.run(os.getenv("TOKEN"))
