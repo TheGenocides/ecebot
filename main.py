8162869610abc0e03a54f99d758a54c4eaf81283
TheGenocides <luke.genesis.hyder@gmail.com>
2024-06-26 13:26:18 +0700

    woops

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ from discord.ext import commands
 # Load the .env file, you need to make a .env file with the TOKEN variable 
 load_dotenv()
 
+os.environ["JISHAKU_NO_UNDERSCORE"] = "true"
+os.environ["JISHAKU_RETAIN"] = "true"
+
 bot = commands.Bot(command_prefix="ec!", intents=discord.Intents.all())
 translator = Translator()
 
 @bot.event
 async def on_ready():
+    await bot.load_extension("jishaku")
+    print("Jishaku loaded!")
     print(f"Logged in as {bot.user}")
 
 @bot.command()
@@ -23,11 +28,11 @@ async def ping(ctx):
 async def translate(ctx, *, text = None):
     msg = await ctx.reply("Translating....")
     text = text if text else ((await ctx.channel.fetch_message(ctx.message.reference.message_id)).content)
-    source = translator.detect(text).lang
+    source = "en" if translator.detect(text).lang == "en" else "id"
     translated_to = "en" if source == "id" else "id"
     translated_text = translator.translate(text, src=source, dest = translated_to).text
     embed = discord.Embed(
-        title=f"{translated_to} -> {'en' if translated_to == 'id' else 'id'}",
+        title=f"{source} -> {translated_to}",
         description=translated_text,
         colour=discord.Color.blurple(),
         timestamp=datetime.datetime.now()
